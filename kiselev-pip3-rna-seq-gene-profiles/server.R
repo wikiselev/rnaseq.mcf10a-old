1b757e41a60493a532a6906e672348e54b6252ad
wikiselev <vladimir.kiselev@babraham.ac.uk>
2015-03-27 14:50:08 +0000

    change gene length normalisation to RPKM in shiny app

--- kiselev-pip3-rna-seq-gene-profiles/server.R
+++ kiselev-pip3-rna-seq-gene-profiles/server.R
@@ -6,7 +6,8 @@ shinyServer(
         function(input, output) {
                 # this will load the data only once when the webpage is open
                 d <- readRDS("data/plot-data.rds")
-                glens <- readRDS("data/gene-lengths.rds")
+                glens <- read.csv("data/exon-map-lengths.csv")
+                mean.lib.size <- 21890091
                 validateFunction <- function() {
                         validate(
                                 need(input$geneName != "",
@@ -18,11 +19,9 @@ shinyServer(
                         )
                 }
                 plotFunction <- function(dat) {
+                        ylabel <- "Read counts"
                         if(input$checkbox) {
-                                validateFunction()
-                                length <- unique(glens[glens$ensembl_gene_id == unique(dat$id),]$gene_length)
-                                dat$value <- dat$value/length
-                                dat$sd <- dat$sd/length
+                                ylabel <- "RPKM"
                         }
                         limits <- aes(ymax = value + sd, ymin = value - sd)
                         ggplot(dat, aes(time, value, group = Condition, color = Condition)) +
@@ -30,14 +29,22 @@ shinyServer(
                                 geom_point(size = 3) +
                                 geom_errorbar(limits, size = 0.5, width = 5) +
                                 facet_grid(. ~ hgnc_symbol) +
-                                labs(x = "Time, min", y = "Read counts") +
+                                labs(x = "Time, min", y = ylabel) +
                                 theme_bw()
                 }
                 data <- reactive({
-                        validateFunction()
-                        d[(d$id %in% toupper(input$geneName)) | (d$hgnc_symbol %in% toupper(input$geneName))]
+                        if(input$checkbox) {
+                                validateFunction()
+                                t <- d[(d$id %in% toupper(input$geneName)) | (d$hgnc_symbol %in% toupper(input$geneName))]
+                                length <- unique(glens[glens$ensembl_gene_id == unique(t$id),]$exon_map_length)
+                                t$value <- t$value / length * 1e9 / mean.lib.size
+                                t$sd <- t$sd / length * 1e9 / mean.lib.size
+                                t
+                        } else {
+                                validateFunction()
+                                d[(d$id %in% toupper(input$geneName)) | (d$hgnc_symbol %in% toupper(input$geneName))]
+                        }
                 })
-                # the function below will be executed each time a user presses goButton
                 output$plot <- renderPlot({
                         plotFunction(data())
                 }, height=400, width = 700)
--- kiselev-pip3-rna-seq-gene-profiles/ui.R
+++ kiselev-pip3-rna-seq-gene-profiles/ui.R
@@ -1,17 +1,13 @@
 library(shiny)
 library(ggplot2)
- 
-# dataset <- diamonds
 
-# dataset <- readRDS("plot-time-courses-all-genes.rds")
- 
 shinyUI(pageWithSidebar(
         headerPanel(
                 "Gene expression profiles in MCF10A cells upon EGF stimulation"
         ),
         sidebarPanel(
                 textInput('geneName', 'Gene HGNC symbol / Ensembl ID'),
-                checkboxInput("checkbox", label = "Normalize by gene length", value = FALSE),
+                checkboxInput("checkbox", label = "RPKM", value = FALSE),
                 # checkboxInput('savePlot', "Save the plot as PDF"),
                 # actionButton("goButton", "Plot!"),
                 div("Please click on links below only after the plot appears on the right.", style = "color:black"),
@@ -23,7 +19,7 @@ shinyUI(pageWithSidebar(
         mainPanel(
                 "This is a Supplementary figure for the paper...",
                 div("\n"),
-                "(read counts are normalized by library sizes using estimateSizeFactors() from DESeq2 library)",
+                "(read counts are normalized by library sizes)",
                 plotOutput('plot')
         )
 ))
